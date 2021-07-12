0c0693aab0c2f3747ac75441241578f586a96c29
Anatoly Burov <anatoly.rr@gmail.com>
2021-07-12 22:34:31 +0300

    Disable saving to yjs from backend to avoid doubling

--- bin/leveldb-persistence-bridge.js
+++ bin/leveldb-persistence-bridge.js
@@ -12,12 +12,17 @@ module.exports = {
     sharedObjectName,
     delta
   ) {
-    console.log('saving to yjs')
-    console.log(documentName, sharedObjectName, delta)
-    const leveldbPersistenceInstance = this.getLeveldbPersistenceInsatnce()
-    await leveldbPersistenceInstance.clearDocument(documentName) // need to ivestigate how to clear single shared object, not entire doc
-    const doc = await getYDoc(documentName)
-    await doc.getText(sharedObjectName).applyDelta(delta)
+    console.log('saving to yjs is disabled')
+    return
+
+    // This code does not work properly: it does not use what's already in leveldb
+    // and also results in document content doubling (since it is not cleared).
+
+    // console.log(documentName, sharedObjectName, delta)
+    // const leveldbPersistenceInstance = this.getLeveldbPersistenceInsatnce()
+    // await leveldbPersistenceInstance.clearDocument(documentName) // need to ivestigate how to clear single shared object, not entire doc
+    // const doc = await getYDoc(documentName)
+    // await doc.getText(sharedObjectName).applyDelta(delta)
 
   },
   getLeveldbPersistenceInsatnce: function () {
